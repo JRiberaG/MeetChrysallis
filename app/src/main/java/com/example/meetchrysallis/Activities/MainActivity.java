4ef85bb3fa3bfec93c0c68a7049fe033be7154a8
JRiberaG <51130683+JRiberaG@users.noreply.github.com>
2020-02-20 13:46:31 +0100

    Prueba

--- app/src/main/java/com/example/meetchrysallis/Activities/MainActivity.java
+++ app/src/main/java/com/example/meetchrysallis/Activities/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.meetchrysallis.Models.Socio;
 import com.example.meetchrysallis.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Socio socio;
     }
 }
