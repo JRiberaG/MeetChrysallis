e8e4e0e3e5f81051ee516e1bb3a11062d7a99ee3
JRiberaG <51130683+JRiberaG@users.noreply.github.com>
2020-02-20 10:42:17 +0100

    Nuevos packages

--- app/src/main/java/com/example/meetchrysallis/Activities/MainActivity.java
+++ app/src/main/java/com/example/meetchrysallis/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.meetchrysallis;
+package com.example.meetchrysallis.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.meetchrysallis.R;
 
 public class MainActivity extends AppCompatActivity {
 
